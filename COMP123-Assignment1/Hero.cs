97ec8d95659635c721132959900fe9424b4560be
oreeba <oreebabadar@gmail.com>
2016-06-01 00:08:06 -0400

    Adding private instance variables and public properties, also started with the constructor method.

---
+++ COMP123-Assignment1/Hero.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment1
+{
+    class Hero
+    {
+        //PRIVATE INSTANCE VARIABLES ***************************************************
+        private int _strength;
+        private int _speed;
+        private int _health;
+        private string _name;
+
+        // PUBLIC PROPERTIES ***********************************************************
+        /**
+         *<summary>
+         *This is a property for our _name field
+         *</summary>
+         *@property {string} Name
+         */
+         public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+           set
+            {
+                this._name = value; 
+            }
+        }
+        // constructor method
+        public Hero (String name)
+        {
+            generateAbilities()
+        }
+
+
+
+
+    }
+}
--- COMP123-Assignment1/Program.cs
+++ COMP123-Assignment1/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace COMP123_Assignment1
 {
+    /* Last modified: 01/06/2016
+     */
     class Program
+    
     {
         static void Main(string[] args)
         {
